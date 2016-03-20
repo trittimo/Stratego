22a6c2a62ebb62929288c9d96836daf32eca1a2c
sbrand83 <brandsm@rose-hulman.edu>
2016-03-20 16:22:12 -0400

    testGetPieces passes

--- src/main/java/stratego/logic/Board.java
+++ src/main/java/stratego/logic/Board.java
@@ -1,5 +1,15 @@
 package stratego.logic;
 
 public class Board {
+
+	private Piece[][] pieces;
+	
+	public Board(Piece[][] pieces) {
+		this.pieces = pieces;
+	}
+	
+	public Piece[][] getPieces(){
+		return pieces;
+	}
 	
 }
