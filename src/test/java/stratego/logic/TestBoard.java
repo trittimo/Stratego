e92d04a363d62292b81e907c88cb2fa7f6a2cee3
sbrand83 <brandsm@rose-hulman.edu>
2016-03-22 19:35:57 -0400

    testPlacePiece tests fail

--- src/test/java/stratego/logic/TestBoard.java
+++ src/test/java/stratego/logic/TestBoard.java
@@ -16,9 +16,26 @@ public class TestBoard {
 	@Test
 	public void testPlacePiece() {
 		Piece[][] pieces = new Piece[3][3];
+		Piece[][] piecestest = new Piece[3][3];
 		Board b = new Board(pieces);
 		Piece p = new Piece(10, 0);
 		b.placePiece(0, 0, p);
-		assertArrayEquals(b.getPieces(), pieces);
+		piecestest[0][0] = p;
+		assertEquals(b.getPieces()[0][0].getValue(), piecestest[0][0].getValue());
+	}
+	
+	@Test
+	public void testPlacePiece2() {
+		Piece[][] pieces = new Piece[3][3];
+		Piece[][] piecestest = new Piece[3][3];
+		Board b = new Board(pieces);
+		Piece p = new Piece(10, 0);
+		b.placePiece(0, 0, p);
+		piecestest[0][0] = new Piece(10, 0);
+		assertArrayEquals(b.getPieces(), piecestest);
+		Piece p2 = new Piece(5, 1);
+		b.placePiece(0, 1, p2);
+		piecestest[0][1] = new Piece(5, 1);
+		assertArrayEquals(b.getPieces(), piecestest);
 	}
 }
