0bf9bd4a24b691832d2f6db513d188ab26c81783
sbrand83 <brandsm@rose-hulman.edu>
2016-03-23 22:58:43 -0400

    testRemovePiece2 passes

--- src/main/java/stratego/logic/Board.java
+++ src/main/java/stratego/logic/Board.java
@@ -13,19 +13,16 @@ public class Board {
 	}
 
 	public void placePiece(int r, int c, Piece p) {
-		// TODO Auto-generated method stub
 		this.pieces[r][c] = p;
 	}
 
 	public void movePiece(int r1, int c1, int r2, int c2) {
-		// TODO Auto-generated method stub
 		this.pieces[r2][c2] = this.pieces[r1][c1];
 		this.pieces[r1][c1] = null;
 	}
 
 	public void removePiece(int r, int c) {
-		// TODO Auto-generated method stub
-		this.pieces[0][0] = null;
+		this.pieces[r][c] = null;
 	}
 	
 }
