83b427b7e47c0cb7b53689b6ab90d3e0655ed5a4
Vibha Alangar <alangavr@rose-hulman.edu>
2016-03-27 15:50:46 -0400

    addPieceToBoardTest fails

--- src/test/java/stratego/logic/TestGame.java
+++ src/test/java/stratego/logic/TestGame.java
@@ -1,6 +1,6 @@
 package stratego.logic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,5 +16,31 @@ public class TestGame {
 		
 		assertEquals(b1.pieceCount(), b2.pieceCount());
 	}
+	
+	@Test
+	public void addPieceToBoardTest() {
+		Game g = new Game();
+		Board b1 = g.getBoard();
+		
+		Piece[][] p = new Piece[10][10];
+		Board b2 = new Board(p);
+		
+		assertEquals(b1.pieceCount(), b2.pieceCount());
+		
+		Piece p1 = new Piece (1, 1);
+		b1.placePiece(3, 0, p1);
+		
+		b2.placePiece(3, 0, p1);
+		
+		assertEquals(g.getBoard().pieceCount(), b2.pieceCount());
+		
+		
+		
+		
+		
+	}
+	
+
+	
 
 }
