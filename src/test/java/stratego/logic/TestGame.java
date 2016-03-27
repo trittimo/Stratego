0ab375ccef4010215c172e223c0d86c09f14d87f
sbrand83 <brandsm@rose-hulman.edu>
2016-03-27 15:30:32 -0400

    testBoardEmpty fails

--- src/test/java/stratego/logic/TestGame.java
+++ src/test/java/stratego/logic/TestGame.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class TestGame {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBoardEmpty() {
+		Game g = new Game();
+		Board b1 = g.getBoard();
+		
+		Piece[][] p = new Piece[10][10];
+		Board b2 = new Board(p);
+		
+		assertEquals(b1.pieceCount(), b2.pieceCount());
 	}
 
 }
