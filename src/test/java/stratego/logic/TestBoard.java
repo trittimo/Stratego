4cfd797cdb1d7b5c71fdc580033e3ad5097ba4eb
Vibha Alangar <alangavr@rose-hulman.edu>
2016-04-13 20:14:52 -0400

    reordering

--- src/test/java/stratego/logic/TestBoard.java
+++ src/test/java/stratego/logic/TestBoard.java
@@ -101,6 +101,28 @@ public class TestBoard {
 	
 	
 	
+	@Test
+	public void testPlacePiece() {
+		Piece[][] pieces = new Piece[3][3];
+		Piece[][] piecestest = new Piece[3][3];
+		Board b = new Board(pieces);
+		Piece p = new Piece(10, 1);
+		b.placePiece(0, 0, p);
+		piecestest[0][0] = p;
+		assertEquals(b.getPieces()[0][0].getValue(), piecestest[0][0].getValue());
+	}
+	
+	@Test
+	public void testPlacePiece2() {
+		Piece[][] pieces = new Piece[3][3];
+		Board b = new Board(pieces);
+		Piece p = new Piece(10, 1);
+		b.placePiece(0, 0, p);
+		assertEquals(b.getPieces()[0][0].getValue(), p.getValue());
+		Piece p2 = new Piece(5, 1);
+		b.placePiece(0, 1, p2);
+		assertEquals(b.getPieces()[0][1].getValue(), p2.getValue());
+	}
 
 	@Test
 	public void testPlacementOutsideBoard() {
@@ -186,28 +208,6 @@ public class TestBoard {
 	}
 
 
-	@Test
-	public void testPlacePiece() {
-		Piece[][] pieces = new Piece[3][3];
-		Piece[][] piecestest = new Piece[3][3];
-		Board b = new Board(pieces);
-		Piece p = new Piece(10, 1);
-		b.placePiece(0, 0, p);
-		piecestest[0][0] = p;
-		assertEquals(b.getPieces()[0][0].getValue(), piecestest[0][0].getValue());
-	}
-
-	@Test
-	public void testPlacePiece2() {
-		Piece[][] pieces = new Piece[3][3];
-		Board b = new Board(pieces);
-		Piece p = new Piece(10, 1);
-		b.placePiece(0, 0, p);
-		assertEquals(b.getPieces()[0][0].getValue(), p.getValue());
-		Piece p2 = new Piece(5, 1);
-		b.placePiece(0, 1, p2);
-		assertEquals(b.getPieces()[0][1].getValue(), p2.getValue());
-	}
 
 	@Test
 	public void testMove1() {
