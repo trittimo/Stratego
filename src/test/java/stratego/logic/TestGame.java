988822f82810068d904062555b47c49e244c7bda
sbrand83 <brandsm@rose-hulman.edu>
2016-04-14 21:00:18 -0400

    mocked more old tests in TestGame

--- src/test/java/stratego/logic/TestGame.java
+++ src/test/java/stratego/logic/TestGame.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.easymock.EasyMock;
 import org.junit.Test;
 
+import stratego.logic.exceptions.InvalidMovement;
 import stratego.logic.exceptions.InvalidPiece;
 
 public class TestGame {
@@ -14,8 +16,17 @@ public class TestGame {
 	@Test
 	public void testInvalidMoveWithNullPiece() {
 		Piece[][] p = new Piece[10][10];
-		Board b1 = new Board(p);
-		Game g = new Game(b1);
+		
+		Board fakeBoard = EasyMock.niceMock(Board.class);
+		
+		Game g = new Game(fakeBoard);
+		
+		EasyMock.expect(fakeBoard.isOccupied(1, 1)).andReturn(false);
+		fakeBoard.movePiece(0, 0, 1, 1);
+		EasyMock.expectLastCall().andThrow(new InvalidMovement("fakePiece", 0, 0, 1, 1));
+		
+		EasyMock.replay(fakeBoard);
+		
 
 		boolean testPasses = false;
 		try {
@@ -24,52 +35,76 @@ public class TestGame {
 			testPasses = e instanceof InvalidPiece;
 		}
 		assertTrue(testPasses);
+		
+		EasyMock.verify(fakeBoard);
 	}
 
 	@Test
 	public void testInvalidMoveOffBoard() {
 		Piece[][] p = new Piece[10][10];
-		Board b1 = new Board(p);
-		Game g = new Game(b1);
+		//Board b1 = new Board(p);
+		Board fakeBoard = EasyMock.niceMock(Board.class);
+		Game g = new Game(fakeBoard);
 
-		Piece p1 = new Piece(5, 1);
-
-		g.getBoard().placePiece(0, 0, p1);
+		//Piece p1 = new Piece(5, 1);
+		Piece fakePiece = EasyMock.niceMock(Piece.class);
+		
+		fakeBoard.placePiece(0, 0, fakePiece);
+		
+		EasyMock.expect(fakeBoard.isOccupied(-1, -1)).andThrow(new IndexOutOfBoundsException());
+		
+		EasyMock.replay(fakeBoard);
 
 		assertFalse(g.makeMove(0, 0, -1, -1));
+		
+		EasyMock.verify(fakeBoard);
 	}
 
 	@Test
 	public void testBoardEmpty() {
 
-		Piece[][] p1 = new Piece[10][10];
-		Board b1 = new Board(p1);
-		Game g = new Game(b1);
+		Board fakeBoard1 = EasyMock.niceMock(Board.class);
+		Game g = new Game(fakeBoard1);
 
-		Piece[][] p = new Piece[10][10];
-		Board b2 = new Board(p);
+		Board fakeBoard2 = EasyMock.niceMock(Board.class);
+		
+		EasyMock.expect(fakeBoard1.pieceCount()).andReturn(0);
+		EasyMock.expect(fakeBoard2.pieceCount()).andReturn(0);
+		
+		EasyMock.replay(fakeBoard1, fakeBoard2);
 
-		assertEquals(b1.pieceCount(), b2.pieceCount());
+		assertEquals(g.getBoard().pieceCount(), fakeBoard2.pieceCount());
+		
+		EasyMock.verify(fakeBoard1, fakeBoard2);
 	}
 
 	@Test
 	public void addPieceToBoardTest() {
 
 		Piece[][] p1 = new Piece[10][10];
-		Board b1 = new Board(p1);
-		Game g = new Game(b1);
+		//Board b1 = new Board(p1);
+		Board fakeBoard1 = EasyMock.niceMock(Board.class);
+		Game g = new Game(fakeBoard1);
 
 		Piece[][] p = new Piece[10][10];
-		Board b2 = new Board(p);
+		//Board b2 = new Board(p);
+		Board fakeBoard2 = EasyMock.niceMock(Board.class);
 
-		assertEquals(b1.pieceCount(), b2.pieceCount());
+		//Piece p2 = new Piece(1, 1);
+		Piece fakePiece = EasyMock.niceMock(Piece.class);
+		
+		fakeBoard1.placePiece(3, 0, fakePiece);
 
-		Piece p2 = new Piece(1, 1);
-		b1.placePiece(3, 0, p2);
+		fakeBoard2.placePiece(3, 0, fakePiece);
+		
+		EasyMock.expect(fakeBoard1.pieceCount()).andReturn(1);
+		EasyMock.expect(fakeBoard2.pieceCount()).andReturn(1);
+		
+		EasyMock.replay(fakeBoard1, fakeBoard2);
 
-		b2.placePiece(3, 0, p2);
-
-		assertEquals(g.getBoard().pieceCount(), b2.pieceCount());
+		assertEquals(g.getBoard().pieceCount(), fakeBoard2.pieceCount());
+		
+		EasyMock.verify(fakeBoard1, fakeBoard2);
 	}
 
 	@Test
