a2aa1007edb38d67964ee5145d6ad38b68cc7ee8
Vibha Alangar <alangavr@rose-hulman.edu>
2016-04-27 19:19:03 -0400

    Added allTests (4 tests fail...we don't know what they are)

---
+++ src/test/java/stratego/logic/AllTests.java
@@ -0,0 +1,25 @@
+package stratego.logic;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestBoard.class, TestGame.class, TestIntegration.class, TestPiece.class})
+
+
+public class AllTests {
+	public static void main (String args[]) {
+        org.junit.runner.JUnitCore.main("stratego.logic.AllTests");
+    }
+	
+	
+	
+	
+	
+	
+
+}
