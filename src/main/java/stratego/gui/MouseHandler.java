d46b8a4d08fd5af92f51262c798477bd6900eafb
sbrand83 <brandsm@rose-hulman.edu>
2016-04-27 23:44:08 -0400

    gui now checks if placed piece is in river or out of first 4 rows at start

--- src/main/java/stratego/gui/MouseHandler.java
+++ src/main/java/stratego/gui/MouseHandler.java
@@ -33,6 +33,8 @@ public class MouseHandler implements MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		
+		//if you clicked on a piece
+		System.out.println("clicked on GUIPiece");
 		if (e.getComponent() instanceof GUIPiece){
 			
 			if (e.getComponent().getParent() instanceof GUIPieceSelector) {
@@ -46,12 +48,16 @@ public class MouseHandler implements MouseListener {
 			Point p = map.getLocation(e.getComponent().getParent()); 
 			int x = (int) p.getX(); 
 			int y = (int) p.getY();
+				//checks if the location on the board that is clicked is occupied by a piece
 				if (game.getBoard().isOccupied(x, y)) {
+					//checks if piece previously selected on map is a piece - starts an attack if so
 					if (mapSelectedPiece != null) {
 						// TODO attack
 						mapSelectedPiece.setSelected(false);
 					}
+					//set currently selected piece as mapSelectedPiece
 					mapSelectedPiece = map.getPiece(x, y);
+					//if where selected is an actual piece
 					if (mapSelectedPiece != null) {
 						mapSelectedPiece.setSelected(true);
 						mapSelectedX = x;
@@ -59,14 +65,20 @@ public class MouseHandler implements MouseListener {
 					}
 				}
 			}
-			
+		
+		//if click on the map, called after click on GUIPiece
 		} else if (e.getComponent() instanceof GUIMap) {
+			System.out.println("clicked on GUIMap");
 			int x = (int) (((double) e.getX()) / ((double) Constants.Dimensions.BOARD_WIDTH) * 10);
 			int y = (int) (((double) e.getY()) / ((double) Constants.Dimensions.BOARD_WIDTH) * 10);
 			
 			if (selectedPiece != null) {
 				selectedPiece.setSelected(false);
-				if (!game.getBoard().isOccupied(x, y)) {
+				//if the space is not occupied and it is a valid location on the board, then place piece
+				System.out.println("checking if piece can be placed there. Occupied?: " + game.getBoard().isOccupied(x, y) +
+						" Valid place?: " + game.getBoard().isValidToPlacePiece(x, y, game.whoseTurn()));
+				if (!game.getBoard().isOccupied(x, y) && game.getBoard().isValidToPlacePiece(x, y, game.whoseTurn())) {
+					System.out.println("placed new piece: " + selectedPiece.getPiece().getPieceName());
 					game.getBoard().placePiece(x, y, selectedPiece.getPiece());
 					map.addPiece(selectedPiece, x, y);
 					selector.removePiece(selectedPiece);
